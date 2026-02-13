b876d69fd31a60a041b38368dc35ee1dd985f2c7
Max <111196993+NqMax@users.noreply.github.com>
2026-02-13 00:21:37 -0600

    feat(backend): add additional middleware configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,20 @@
 import express from "express";
+import { apiRouter } from "@/routes/api";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-const port = 3000;
+app.use(cors({
+  origin: "http://127.0.0.1:5173",
+  credentials: true,
+}));
+app.use(cookieParser());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api", apiRouter);
+
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);

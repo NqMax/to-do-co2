7e2c04d61dfdb384197ba5871b5837f925325b95
Max <111196993+NqMax@users.noreply.github.com>
2026-02-14 14:49:04 -0600

    feat: add initial task schema

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,13 +1,36 @@
-import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgTable,
+  varchar,
+  timestamp,
+  pgEnum,
+} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   email: varchar().notNull().unique(),
   password: varchar().notNull(),
   salt: varchar().notNull(),
-  department: varchar().notNull().references(() => departments.name),
-  role: varchar().notNull().references(() => roles.name),
+  department: varchar()
+    .notNull()
+    .references(() => departments.name),
+  role: varchar()
+    .notNull()
+    .references(() => roles.name),
   createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+});
+
+const statusEnum = pgEnum("taskStatus", ["pending", "inProgress", "completed"]);
+const priorityEnum = pgEnum("taskPriority", ["low", "medium", "high"]);
+export const task = pgTable("tasks", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar().notNull(),
+  description: varchar().notNull(),
+  status: statusEnum().notNull(),
+  priority: priorityEnum().notNull(),
+  createdBy: integer().references(() => usersTable.id),
+  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+  updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
 });
 
 export const departments = pgTable("departments", {
